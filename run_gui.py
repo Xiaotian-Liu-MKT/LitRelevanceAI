09b7d4baec8c49fbb655206906ec317b5de43280
Xiaotian Liu <49262952+Xiaotian-Liu-MKT@users.noreply.github.com>
2025-09-01 21:32:04 +1000

    Add convenience script for launching GUI

---
+++ run_gui.py
@@ -0,0 +1,10 @@
+"""Convenience script to launch the LitRx graphical interface.
+
+Run:
+    python run_gui.py
+"""
+from litrx.gui.main_window import launch_gui
+
+
+if __name__ == "__main__":
+    launch_gui()
